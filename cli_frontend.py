e7e2992c49c8afdf9f02529807c17c1d2547f079
Fennix <n303p4@noreply.gitlab.users.com>
2021-09-07 09:38:07 -0400

    Rewrite text bot as a client for http_backend.py

---
+++ cli_frontend.py
@@ -0,0 +1,50 @@
+"""A command-line frontend for http_backend.py."""
+
+import json
+import time
+
+import requests
+
+
+def get_prefix(text, prefix):
+    """If a text string starts with a substring, return the substring and the text minus the first instance of the
+    substring; otherwise return None and the text.
+    """
+    if text.startswith(prefix):
+        return prefix, text[len(prefix):].lstrip()
+    return None, text
+
+
+def main():
+    """Main method to start the bot."""
+
+    with open("config.json") as config_file:
+        config = json.load(config_file)
+
+    prefix = config.get("prefix", "")
+    backend_port_number = config.get("backend_port_number", 9980)
+    backend_url = f"http://localhost:{backend_port_number}"
+
+    print(f"Enter commands here. Commands must start with {prefix}")
+    prefix_space = " " if len(prefix) > 1 else ""
+    print(f"For help, use {prefix}{prefix_space}help.")
+
+    while True:
+        prefix, message = get_prefix(input("> "), prefix)
+        if prefix:
+            try:
+                response = requests.post(backend_url, json={
+                    "id": f"cli:{time.time()}",
+                    "message": message,
+                    "is_owner": True,
+                    "character_limit": 0
+                })
+                replies = response.json()
+                for reply in replies:
+                    print(reply)
+            except Exception:
+                print("Error: http_backend.py is not running!")
+
+
+if __name__ == "__main__":
+    main()
--- cogs/owner/process.py
+++ cogs/owner/process.py
@@ -1,24 +1,18 @@
 """Commands that affect the bot's execution."""
 
-import sys
-import os
-
 from sailor import commands
 
 
 @commands.command(aliases=["exit", "shutdown", "kys"], owner_only=True)
 async def halt(event):
     """Halt the bot. Owner only."""
+
     if event.invoked_with == "kys":
         await event.reply("Dead! x.x")
     else:
         await event.reply("Good night~")
-    await event.processor.logout()
 
+    def logout():
+        event.processor.loop.create_task(event.processor.logout())
 
-@commands.command(owner_only=True)
-async def restart(event):
-    """Restart the bot. Owner only."""
-    await event.reply("I'll be back soon~")
-    await event.processor.logout()
-    os.execl(sys.executable, sys.executable, *sys.argv)
+    event.processor.loop.call_later(1, logout)
