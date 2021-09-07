ff37b8e0da403787bd3d311173fc77fd0474b2c9
Fennix <n303p4@noreply.gitlab.users.com>
2021-09-07 16:14:52 -0400

    Document ping better

--- cogs/core/ping.py
+++ cogs/core/ping.py
@@ -6,5 +6,9 @@ from sailor import commands
 @commands.cooldown(6, 12)
 @commands.command()
 async def ping(event):
-    """Ping the bot."""
+    """
+    Ping the bot.
+
+    The main purpose of this command is debugging, to make sure the bot is alive.
+    """
     await event.reply(":3")
