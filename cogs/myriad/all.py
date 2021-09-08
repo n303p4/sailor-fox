43e7f7f461481aabc6ffa4bbdfce296e3d7dfb69
Fennix <n303p4@noreply.gitlab.users.com>
2021-09-08 02:52:29 -0400

    Update Myriad stub to support module teardowns

--- cogs/myriad/all.py
+++ cogs/myriad/all.py
@@ -6,3 +6,8 @@ from myriad import sailor_cogs
 def setup(processor):
     """Set up the cogs."""
     sailor_cogs.setup(processor)
+
+
+def unload(processor):
+    """Tear down the cogs."""
+    sailor_cogs.unload(processor)
