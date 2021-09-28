6ba476d5963e3feddb654d718c50a7f3b6bac2e6
Fennix <n303p4@noreply.gitlab.users.com>
2021-09-27 23:07:46 -0400

    Lol

--- cogs/democ/thread_name.py
+++ cogs/democ/thread_name.py
@@ -5,11 +5,11 @@ from sailor import commands
 
 @commands.cooldown(6, 12)
 @commands.command(aliases=["cn", "title", "threadname", "tn", "thread"])
-async def channelname(ctx, *, channel_name: str):
+async def channelname(event, *, channel_name: str):
     """
     Change the name of a channel.
 
     As it is incredibly abusable, this command should not be used in a public bot, ever.
     """
-    await ctx.send(f"**{channel_name}**")
-    await ctx.channel.edit(name=channel_name)
+    await event.reply(f"**{channel_name}**")
+    await event.channel.edit(name=channel_name)
