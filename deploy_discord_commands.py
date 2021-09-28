85786193cb948c18340725141c521c56218cc79f
Fennix <n303p4@noreply.gitlab.users.com>
2021-09-27 20:16:37 -0400

    Deploy all commands as slash commands

--- deploy_discord_commands.py
+++ deploy_discord_commands.py
@@ -1,7 +1,8 @@
-"""Deploy sailor-fox slash command. Just one."""
+"""Deploy sailor-fox slash command. Pretty much just a hack."""
 
 # pylint: disable=invalid-name
 
+import asyncio
 import sys
 
 import aiohttp
@@ -14,15 +15,17 @@ logger = create_logger(__name__)
 
 
 def main():
-    """Factory that creates a Discord client and uses it to create a single application command."""
+    """Factory that creates a Discord client and uses it to register slash commands."""
 
     client = discord.Client()
     processor = ProcessorWithConfig(loop=client.loop)
     processor.load_config()
+    assert (isinstance(processor.config.get("module_blocklist", []), list)), "Blocklist must be a list."
+    processor.add_modules_from_dir("cogs", blocklist=processor.config.get("module_blocklist", []))
 
     @client.event
     async def on_ready():
-        """When ready, deploy command."""
+        """When ready, deploy commands."""
 
         if not processor.session:
             processor.session = aiohttp.ClientSession(loop=processor.loop)
@@ -36,6 +39,29 @@ def main():
         headers = {
             "Authorization": f"Bot {processor.config['discord_token']}"
         }
+
+        for index, command in enumerate(processor.commands.values(), start=1):
+            request_payload = {
+                "name": command.name,
+                "type": 1,
+                "description": " ".join(command.help.split("\n"))[:100][0],
+                "options": [
+                    {
+                        "name": "input",
+                        "description": f"Run /help {command.name} for more details",
+                        "type": 3,
+                        "required": False
+                    }
+                ]
+            }
+            logger.info("Registering %s (%s of %s)...", command.name, index, len(processor.commands))
+            async with processor.session.post(url, headers=headers, json=request_payload) as response:
+                if response.status >= 400:
+                    logger.error(response)
+                    logger.warning("Command not registered (HTTP %s)", response.status)
+                else:
+                    logger.info("Command registered (HTTP %s)", response.status)
+            await asyncio.sleep(5)
 
         request_payload = {
             "name": processor.config["discord_slash_prefix"],
