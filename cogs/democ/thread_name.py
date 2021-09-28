311aebe9a054f50e53e20bc33ef02cedb26e80ef
Fennix <n303p4@noreply.gitlab.users.com>
2021-09-27 23:53:44 -0400

    Update thread_name

--- cogs/democ/thread_name.py
+++ cogs/democ/thread_name.py
@@ -1,4 +1,8 @@
-"""Edit the title of a channel."""
+"""
+Change the name of a channel.
+
+As it is incredibly abusable, this command should not be used in a public bot, ever.
+"""
 
 from sailor import commands
 
@@ -6,10 +10,6 @@ from sailor import commands
 @commands.cooldown(6, 12)
 @commands.command(aliases=["cn", "title", "threadname", "tn", "thread"])
 async def channelname(event, *, channel_name: str):
-    """
-    Change the name of a channel.
-
-    As it is incredibly abusable, this command should not be used in a public bot, ever.
-    """
-    await event.reply(f"**{channel_name}**")
+    """Change the name of a channel."""
+    await event.reply(event.f.bold(channel_name))
     await event.channel.edit(name=channel_name)
