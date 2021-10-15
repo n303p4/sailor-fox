68fe572bb7d371f7740b9258429b2c502b06bdba
Fennix <n303p4@noreply.gitlab.users.com>
2021-10-15 02:58:12 -0400

    Allow optional number to be provided for reactions

--- cogs/reactions/reactions.py
+++ cogs/reactions/reactions.py
@@ -4,6 +4,7 @@ import json
 import secrets
 
 from sailor import commands
+from sailor.exceptions import UserInputError
 
 
 def setup(processor):
@@ -14,7 +15,7 @@ def setup(processor):
 
     last_image_for_command = {}
 
-    async def coro(event):
+    async def coro(event, image_number: int = None):
         """Generic coroutine."""
         command_properties = reactions.get(event.command.name)
         message = command_properties.get("message")
@@ -24,15 +25,33 @@ def setup(processor):
         if not isinstance(images, list):
             return
         # Avoid repeats when possible
-        image = secrets.choice(images)
-        while image == last_image_for_command.get(event.command.name):
+        if len(images) > 1 and isinstance(image_number, int):
+            image_number -= 1
+            if image_number not in range(len(images)):
+                raise UserInputError(f"Number must be from 1 to {len(images)}")
+            image = images[image_number]
+        else:
             image = secrets.choice(images)
-        if len(images) > 1:
-            last_image_for_command[event.command.name] = image
+            while image == last_image_for_command.get(event.command.name):
+                image = secrets.choice(images)
+            if len(images) > 1:
+                last_image_for_command[event.command.name] = image
         await event.reply(image)
+
+    async def coro_noselect(event):
+        """Generic coroutine, without manual image selection."""
+        await coro(event)
 
     for command_name, command_properties in reactions.items():
         aliases = command_properties.get("aliases", [])
-        new_command = commands.Command(coro, name=command_name, aliases=aliases)
-        new_command.help = f"Posts a {command_name}-themed image."
+        num_images = len(command_properties.get("images", []))
+        if num_images > 1:
+            new_command = commands.Command(coro, name=command_name, aliases=aliases)
+            new_command.help = (
+                f"Randomly posts any of {num_images} {command_name}-themed images.\n\n"
+                "A number can be provided to select a specific image."
+            )
+        else:
+            new_command = commands.Command(coro_noselect, name=command_name, aliases=aliases)
+            new_command.help = f"Posts a {command_name}-themed image."
         processor.add_command(new_command)
