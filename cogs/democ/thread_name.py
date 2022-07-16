24489e9fafea773b91f740e83fd24592d0772912
Fennix <n303p4@noreply.gitlab.users.com>
2022-07-15 21:22:48 -0400

    Question marks are replaced with unknown

--- cogs/democ/thread_name.py
+++ cogs/democ/thread_name.py
@@ -4,6 +4,8 @@ Change the name of a channel.
 As it is incredibly abusable, this command should not be used in a public bot, ever.
 """
 
+import re
+
 from sailor import commands
 
 
@@ -11,5 +13,6 @@ from sailor import commands
 @commands.command(aliases=["cn", "title", "threadname", "tn", "thread"])
 async def channelname(event, *, channel_name: str):
     """Change the name of a channel."""
+    channel_name = re.sub(r"\?+", "Unknown", channel_name)
     await event.reply(event.f.bold(channel_name))
     await event.channel.edit(name=channel_name)
